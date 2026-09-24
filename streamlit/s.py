97d65459182d8a02b63d7adce51c04735e7e4c7b
kokaabhay <abhay.koka@winwire.com>
2026-09-24 11:57:57 +0530

    streamlit basic integrated

---
+++ streamlit/s.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import requests
+API_BASE_URL="http://127.0.0.1:8000"
+st.write("Ask about our products")
+#st.session_state.api_client = API_BASE_URL
+q=st.text_input("Query")
+if st.button("Ask question"):
+    if q:
+        response=requests.post(
+            f"{API_BASE_URL}/get_response",
+        
+        json={"query":q})
+        answer=response.json()
+        st.write(answer)
+   
+
